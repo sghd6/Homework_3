32beb821fe1e98b014aa582145b0cd756ee39ba3
sghd6 <142858898+sghd6@users.noreply.github.com>
2024-02-14 17:03:47 -0600

    Update MediaProduct.java
    Getter, Setter, To String methods for MediaProduct Class.

--- M_Homework_3/src/m/mediaProduct/MediaProduct.java
+++ M_Homework_3/src/m/mediaProduct/MediaProduct.java
@@ -13,6 +13,43 @@ public class MediaProduct {
         this.year = year;
         this.genre = genre;
     }
+	
 
-    // Getters, Setters, and a toString method here
+    // Getters, Setters, and a toString methods
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public Genre getGenre() {
+		return genre;
+	}
+
+	public void setGenre(Genre genre) {
+		this.genre = genre;
+	}
+
+	@Override
+	public String toString() {
+		return "MediaProduct [title=" + title + ", price=" + price + ", year=" + year + ", genre=" + genre + "]";
+	}
 }
