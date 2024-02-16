9153a3d61836e3b5b692c769493703c1889ff28f
Jack Riebel <71671813+JackRiebel@users.noreply.github.com>
2024-02-16 15:11:21 -0600

    Update CDRecordProduct.java
    Updated the CDRecordProduct file by adding a couple of methods.

--- M_Homework_3/src/m/mediaProduct/CDRecordProduct.java
+++ M_Homework_3/src/m/mediaProduct/CDRecordProduct.java
@@ -1,10 +1,25 @@
 package m.mediaProduct;
 
 public class CDRecordProduct extends MediaProduct {
+
+    // Constructor that uses attributes inherited from MediaProduct
     public CDRecordProduct(String title, double price, int year, Genre genre) {
         super(title, price, year, genre);
     }
 
-    // Add other CDRecordProduct methods here
-}
+    // Copy constructor to prevent information leaks
+    public CDRecordProduct(CDRecordProduct other) {
+        super(other); // Calls the copy constructor of the superclass (MediaProduct)
+    }
 
+    // Override the toString() method if necessary to include additional CD-specific details
+    @Override
+    public String toString() {
+        return "CDRecordProduct{" +
+                "title='" + title + '\'' +
+                ", price=" + price +
+                ", year=" + year +
+                ", genre=" + genre +
+                '}';
+    }
+}
