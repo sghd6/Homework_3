133a7f407ce871e266ac30a2da353a8e4618b648
Jack Riebel <71671813+JackRiebel@users.noreply.github.com>
2024-02-16 15:16:35 -0600

    Update VinylRecordProduct.java
    Updated ViynlRecordProduct by adding constructors

--- M_Homework_3/src/m/mediaProduct/VinylRecordProduct.java
+++ M_Homework_3/src/m/mediaProduct/VinylRecordProduct.java
@@ -1,10 +1,27 @@
 package m.mediaProduct;
 
 public class VinylRecordProduct extends MediaProduct {
+
+    // Constructor that uses attributes inherited from MediaProduct
     public VinylRecordProduct(String title, double price, int year, Genre genre) {
         super(title, price, year, genre);
     }
 
-    // Add other VinylRecordProduct methods here
+    // Copy constructor to prevent information leaks
+    public VinylRecordProduct(VinylRecordProduct other) {
+        super(other); // Calls the copy constructor of the superclass (MediaProduct)
+    }
+
+    // Override the toString() method if necessary to include additional Vinyl-specific details
+    @Override
+    public String toString() {
+        return "VinylRecordProduct{" +
+                "title='" + title + '\'' +
+                ", price=" + price +
+                ", year=" + year +
+                ", genre=" + genre +
+                '}';
+    }
 }
+
 
