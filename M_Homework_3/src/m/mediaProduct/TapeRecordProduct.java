76315b1d75eaafc292a27c54576b39eb4ed820f1
Jack Riebel <71671813+JackRiebel@users.noreply.github.com>
2024-02-16 15:13:20 -0600

    Update TapeRecordProduct.java
    Updated TapeRecordProduct by adding methods

--- M_Homework_3/src/m/mediaProduct/TapeRecordProduct.java
+++ M_Homework_3/src/m/mediaProduct/TapeRecordProduct.java
@@ -1,10 +1,27 @@
 package m.mediaProduct;
 
 public class TapeRecordProduct extends MediaProduct {
+
+    // Constructor that uses the 'super' keyword to initialize attributes inherited from MediaProduct
     public TapeRecordProduct(String title, double price, int year, Genre genre) {
         super(title, price, year, genre);
     }
 
-    // Add other TapeRecordProduct methods here
+    // Copy constructor to prevent information leaks
+    public TapeRecordProduct(TapeRecordProduct other) {
+        super(other); // Calls the copy constructor of the superclass (MediaProduct)
+    }
+
+    // Override the toString() method if necessary to include additional Tape-specific details
+    @Override
+    public String toString() {
+        return "TapeRecordProduct{" +
+                "title='" + title + '\'' +
+                ", price=" + price +
+                ", year=" + year +
+                ", genre=" + genre +
+                '}';
+    }
 }
+
 
